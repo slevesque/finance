98d82f9043706e79abd596c79c1cfcecf24acc30
Sylvain Lévesque <slevesque@gezere.com>
2018-06-08 13:44:21 -0400

    Complete basic symbol search

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,16 @@
+class StocksController < ApplicationController
+  def search
+    if params[:stock].present? 
+      @stock = Stock.new_from_lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:danger] = 'You have entered and incorrect symbol'
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:danger] = 'You have entered and emtpy search string';
+      redirect_to my_portfolio_path
+    end
+  end
+end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,15 @@
 class Stock < ApplicationRecord
+  def self.new_from_lookup(ticker_symbol)
+    begin
+      ret = StockQuote::Stock.quote(ticker_symbol)
+      price = strip_commas(ret.latest_price)
+      new(name: ret.company_name, ticker: ret.symbol, last_price: price)
+    rescue Exception => e
+      return nil
+    end
+  end
+
+  def self.strip_commas(number)
+    number.to_s.gsub(",", "")
+  end
 end
