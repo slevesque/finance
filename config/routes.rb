d23d8497c158f1a206da660385603ceecb1d4f8b
Sylvain Lévesque <sylvainl@demarque.ca>
2018-06-12 12:21:48 -0400

    Fix resources with an S

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
-  resource :user_stocks, only: [:create, :destroy]
+  resources :user_stocks, only: [:create, :destroy]
 end
